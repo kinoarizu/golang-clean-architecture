aa6b2b570333024a44f6f3186c330e5085416f21
ルイタイゼン <abuaziscorp@gmail.com>
2024-01-08 17:14:55 +0700

    formated document

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -341,26 +341,26 @@ func main() {
 	// ================================
 
 	/*
-	group := sync.WaitGroup{}
-	for i := 0; i < 100; i++ {
-		group.Add(1)
-		go func() {
-			time.Sleep(3 * time.Second)
-			group.Done()
-		}()
-	}
+		group := sync.WaitGroup{}
+		for i := 0; i < 100; i++ {
+			group.Add(1)
+			go func() {
+				time.Sleep(3 * time.Second)
+				group.Done()
+			}()
+		}
 
-	totalCpu := runtime.NumCPU()
-	fmt.Println("Total CPU", totalCpu)
+		totalCpu := runtime.NumCPU()
+		fmt.Println("Total CPU", totalCpu)
 
-	runtime.GOMAXPROCS(20)
-	totalThread := runtime.GOMAXPROCS(-1)
-	fmt.Println("Total Thread", totalThread)
+		runtime.GOMAXPROCS(20)
+		totalThread := runtime.GOMAXPROCS(-1)
+		fmt.Println("Total Thread", totalThread)
 
-	totalGoroutine := runtime.NumGoroutine()
-	fmt.Println("Total Goroutine", totalGoroutine)
+		totalGoroutine := runtime.NumGoroutine()
+		fmt.Println("Total Goroutine", totalGoroutine)
 
-	group.Wait()
+		group.Wait()
 	*/
 }
 
